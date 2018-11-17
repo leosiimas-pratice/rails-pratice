3707b3dc00381de4e4580f0e6eafcc7224c50b13
leosiimas <abreu.leo@hotmail.com>
2018-11-17 14:22:54 -0200

    Aula 3, parte 2
    - Adicionado a gem carrierwave para gerenciar photos
    - Alterado model product para aceitar imagens

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ ruby '2.4.4'
 gem 'rails_admin'
 gem 'devise'
 gem 'cancancan', '~> 1.15.0'
+gem 'carrierwave'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.1'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_17_143647) do
+ActiveRecord::Schema.define(version: 2018_11_17_161609) do
 
   create_table "addresses", force: :cascade do |t|
     t.string "country"
@@ -80,6 +80,7 @@ ActiveRecord::Schema.define(version: 2018_11_17_143647) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.decimal "price"
+    t.string "photo"
   end
 
   create_table "sales", force: :cascade do |t|
