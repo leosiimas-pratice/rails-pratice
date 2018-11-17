9acb4c6a20401f2c096761fee7aa9ae997458d62
leosiimas <abreu.leo@hotmail.com>
2018-11-16 23:43:38 -0200

    Primeira Aula
    - Criação dos models: User, Client, Address, Product, Discounts, Product_Quantative, Sales, Comissions
    - Organizando as relações de cada model
    - Adicionado Gem rails_admin
    - Criação de seeds para popular o banco de dados

---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+#Criando nossos Users --- Obs: Depois que adicionarmos o devise precisamos inccluir o email e senha dos users
+User.create name: 'José', status: :active, kind: :salesman
+User.create name: 'Marcos', status: :active, kind: :manager
+
+#Criando alguns produtos de exemplo
+Product.create name: 'Smartphone', description:'Um smartphone novo ...', status: :active
+Product.create name: 'Tablet', description: 'Um tablet novo ...', status: :active
+
+#Criando um desconto de exemplo
+Discount.create name: 'Desconto carnaval', description: 'Aplique esse descnto no carnaval', value: '10', kind: :porcent, status: :active
